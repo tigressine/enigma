94df2810afff5956ccf06984c065329202163a27
Tiger Sachse <tiger@sachse.io>
2017-11-10 17:57:41 -0500

    db done

--- enigma/database.py
+++ enigma/database.py
@@ -1,4 +1,3 @@
-#! /usr/bin/env python3
 import sqlite3 as sql
 
 F_DAILY_SHEET = 'daily_sheet.db'
@@ -18,28 +17,12 @@ def db_operation(operation):
     return db_wrap
 
 @db_operation
-def read_column(day, column, curse):
-    curse.execute('''SELECT * FROM daily_sheet WHERE day=?''', (day,))
-    row = curse.fetchone()
-    values = {'day' : row[0],
-              'rotor_nums': (row[1], row[2], row[3]),
-              'rotor_start': (row[4], row[5], row[6]),
-              'reflector': row[7],
-              'plugboard': row[8]}
-    return values[column]
-
-
-@db_operation
 def create_database(curse):
-    curse.execute("""CREATE TABLE daily_sheet (day int,
-                                               rotor_inner int,
-                                               rotor_middle int,
-                                               rotor_outer int,
-                                               rotor_inner_start int,
-                                               rotor_middle_start int,
-                                               rotor_outer_start int,
-                                               reflector text,
-                                               plugboard text)""")
+    curse.execute("""CREATE TABLE daily_sheet
+                     (day int, rotor_inner int, rotor_middle int,
+                      rotor_outer int, rotor_inner_start int,
+                      rotor_middle_start int, rotor_outer_start int,
+                      reflector text, plugboard text)""")
 
 @db_operation
 def populate_database(curse):
@@ -47,12 +30,25 @@ def populate_database(curse):
         lines = f.readlines()
         for line in lines:
             data = line.strip().split('|')
-            for i in range(7):
-                data[i] = int(data[i])
-            curse.execute("""INSERT INTO daily_sheet VALUES(?,?,?,?,?,?,?,?,?)""", tuple(data))
+            curse.execute("""INSERT INTO daily_sheet
+                             VALUES(?,?,?,?,?,?,?,?,?)""", tuple(data))
 
-#create_database()
-#populate_database()
+@db_operation
+def read_column(day, column, curse):
+    curse.execute("""SELECT * FROM daily_sheet WHERE day=?""", (day,))
+    row = curse.fetchone()
+    data = {'day' : row[0],
+            'rotor_nums': (row[1], row[2], row[3]),
+            'rotor_start': (row[4], row[5], row[6]),
+            'reflector': row[7],
+            'plugboard': row[8]}
+    
+    return data[column]
 
-print(read_column(2, 'rotor_nums'))
-print(read_column(4, 'plugboard'))
+def print_database():
+    for i in range(1, 32):
+        print(read_column(i, 'day'), ' ', end='')
+        print(read_column(i, 'rotor_nums'), ' ', end='')
+        print(read_column(i, 'rotor_start'), ' ', end='')
+        print(read_column(i, 'reflector'), ' ', end='')
+        print(read_column(i, 'plugboard'), ' ')
