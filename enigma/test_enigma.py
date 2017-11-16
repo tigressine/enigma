f3a6c6dca4cb1f1f58a082de508291c6a0c63b99
Tiger Sachse <tiger@sachse.io>
2017-11-16 17:58:24 -0500

    fixing issues in test suite

--- enigma/test_enigma.py
+++ enigma/test_enigma.py
@@ -2,33 +2,36 @@
 import unittest
 import engine
 import database
+import random
 
 F_TEST_INPUT = 'test_input.txt'
+E = engine.EnigmaMachine()
+with open(F_TEST_INPUT, 'r') as f:
+    remove_newline = lambda x: list(filter(lambda y: y != '', x.split('\n')))
+    SAMPLE = remove_newline(f.read().upper())#temp
+
+def double_translate(message, day=1):
+    return E.translate(E.translate(message, day), day)
 
 class TestEnigma(unittest.TestCase):
     
-    def setUp(self):
-        self.e = engine.EnigmaMachine()
-        with open(F_TEST_INPUT, 'r') as f:
-            self.sample = f.read().upper()#temp
-
     def test_000_bulk_translation(self):
-        self.encoded_text = self.e.translate(self.sample)
-        self.decoded_text = self.e.translate(self.encoded_text)
-        self.assertEqual(self.decoded_text, self.sample)
+        for string in SAMPLE:
+            self.assertEqual(double_translate(string), string)
 
     def test_001_custom_days(self):
-        self.sample = self.sample.split('\n')[0].upper()#temp
-        for i in range(1,32):
-            self.encoded_text = self.e.translate(self.sample, day=i)
-            self.decoded_text = self.e.translate(self.encoded_text, day=i)
-            self.assertEqual(self.decoded_text, self.sample)
+        for i in range(1, 32):
+            sample = random.choice(SAMPLE)
+            self.assertEqual(double_translate(sample, i), sample)
 
+    def test_002_custom_days_wrong(self):
+        for i in range(1, 32):
+            sample = random.choice(SAMPLE)
+            self.assertNotEqual(double_translate(sample[::-1], i), sample)
+    '''
     def test_002_rotor_set(self):
-        '''
         self.e.rotors[0].set(20)
         self.assertEqual(self.e.rotors[0].sequence[0], self.e.rotors[0].original_sequence[20])
-        '''
         self.sample = 'T'
         for i in range(1,32):
             self.e.translate(self.sample, day=i)
@@ -38,7 +41,6 @@ class TestEnigma(unittest.TestCase):
                                                 self.e.rotor_start[self.e.rotor_nums.index(rotor)]]
                 print(first, o_first)
                 self.assertEqual(first, o_first)
-    '''
     def test_002_rotor_rotation(self):
         print(len(self.sample)/27/27)
         self.e.translate(self.sample)
